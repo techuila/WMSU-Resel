ca858bfbaf0bbe8cbfff8b4a926fd6985775d85f
nerdcorez <axeguns@gmail.com>
2018-03-09 02:23:49 +0700

    asda

--- php/login.php
+++ php/login.php
@@ -13,6 +13,7 @@
         // output data of each row
         while($row = $result->fetch_assoc()) {
             $banner = true;
+            $banner['nam'] = $row['nam'];
         
         }
         // print_r($food);
